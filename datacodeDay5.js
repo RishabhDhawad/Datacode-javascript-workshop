801d46c4025001e67232f0c2df4598a71491d976
Rishabh Dhawad <rishabhdhawad29@gmail.com>
2023-01-20 22:08:34 +0530

    update: day5

---
+++ datacodeDay5.js
@@ -0,0 +1,9 @@
+// Data Code Js Workshop  Day 5
+
+// Learnt about the concept of DOM(Document Object Model)
+// API's , JS Engine 
+
+// Also learn about the concepts of Inspect (Console) Element
+
+// Saw the CRUD operations on the Age Calculator.
+// Saw various operations on the Element Objects as well.
